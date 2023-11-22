f845cd831c7d348e20b6f6dffdef182b2c8e8d89
Darknessmanultra <ivan.dorador@alumnos.ucn.cl>
2023-11-22 18:27:19 -0300

    readme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,10 @@
 <html>
 <head>
     <title>Productos</title>
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-
+<button type="button">Post</button>
 <div id="app"></div>
 
 <script src="{{ mix('js/app.js') }}"></script>
